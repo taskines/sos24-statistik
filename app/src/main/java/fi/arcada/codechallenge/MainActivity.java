a345b10c4bd917b1934ccf35aea5581138a96437
taskines <taskines@arcada.fi>
2024-03-25 15:00:31 +0200

    CodeChallenge

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,9 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,42 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView outputText;
+    EditText inputText;
+    double[] values = {1.2, 2.3, 4.5, 6.7, 8.9, 12.9};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        outputText = findViewById(R.id.textView);
+        inputText = findViewById(R.id.editTextText);
+
+        outputText.setText("Min App Funkar");
+    }
+
+
+    private double calcMean(double[] data) {
+        double sum = 0;
+        for (double value : data) {
+            sum += value;
+        }
+        return sum / data.length;
+    }
+
+
+    public void buttonHandler(View view) {
+
+        String userInput = inputText.getText().toString();
+
+
+        outputText.setText(userInput);
+
+
+    }
+    public void buttonHandler2 (View view) {
+        double average = calcMean(values);
+        outputText.append("\nMedeltal: " + average);
     }
 }
